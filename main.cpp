9fa35407b0b83eadd87132446563917a0042edf5
kamilstroy <k.alyautdinov2013@yandex.ru>
2024-06-19 09:43:12 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+//#define s1(param) std::param
+
+/*
+class worki
+{
+private:   
+public:
+
+virtual int work(void) = 0;
+
+};
+
+
+class comandante
+{private:
+s1(string) command;
+
+};
+*/
+
+int main(void){
+system("gdb --help");
+system("cls");
+    return 0;
+}
